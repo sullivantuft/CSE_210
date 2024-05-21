77ae151a0bc0fcdde1d353eb55f5c8e83ac2630b
sullivantuft <tuf19001@byui.edu>
2024-05-20 19:26:46 -0600

    Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("yellow", 10, 15);
+        shapes.Add(rectangle);
+        
+        Circle circle = new Circle("orange", 7);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape has a color of {color} and an area of {area}");
+        }
+
     }
 }
